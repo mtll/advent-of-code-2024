dada13fc91753f82be9420b7e42395257aae7794
david feller <dave.feller@gmail.com>
2024-12-02 09:11:52 -0700

    formating

--- day1.m
+++ day1.m
@@ -8,21 +8,18 @@
 (*2024 Day 1*)
 
 
-(* ::Input:: *)
-(*{i1,i2}=Import["input/1.txt","Data"]\[Transpose];*)
+{i1, i2} = Import["input/1.txt", "Data"]\[Transpose];
 
 
 (* ::Subsection:: *)
 (*Part 1*)
 
 
-(* ::Input:: *)
-(*Total@Abs[Subtract@@(Sort/@{i1,i2})]*)
+Total@Abs[Subtract @@ (Sort /@ {i1, i2})]
 
 
 (* ::Subsection:: *)
 (*Part 2*)
 
 
-(* ::Input:: *)
-(*i1 . Lookup[Counts[i2],i1,0]*)
+i1 . Lookup[Counts[i2], i1, 0]
--- day2.m
+++ day2.m
@@ -8,25 +8,21 @@
 (*2024 Day 2*)
 
 
-(* ::Input:: *)
-(*in=ToExpression@StringSplit@ReadList["input/2.txt","String"];*)
+in = ToExpression@StringSplit@ReadList["input/2.txt", "String"];
 
 
-(* ::Input:: *)
-(*safe[l_]:=-3\[VectorLessEqual]#\[VectorLessEqual]-1||1\[VectorLessEqual]#\[VectorLessEqual]3&[Differences[l]]*)
+safe[l_] := -3 \[VectorLessEqual] # \[VectorLessEqual] -1 || 1 \[VectorLessEqual] # \[VectorLessEqual] 3 &[Differences[l]]
 
 
 (* ::Subsubsection:: *)
 (*Part 1*)
 
 
-(* ::Input:: *)
-(*Count[in,_?safe]*)
+Count[in, _?safe]
 
 
 (* ::Subsubsection:: *)
 (*Part 2*)
 
 
-(* ::Input:: *)
-(*Count[in,_?(AnyTrue[Subsets[#,{Length[#]-1}],safe]&)]*)
+Count[in, _?(AnyTrue[Subsets[#, {Length[#] - 1}], safe]&)]
