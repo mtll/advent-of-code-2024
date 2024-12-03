a0981e40a13afc10d45026d7bed787099fa0ba16
david feller <dave.feller@gmail.com>
2024-12-02 18:50:56 -0700

    golf

--- Common Lisp/day2.lisp
+++ Common Lisp/day2.lisp
@@ -4,16 +4,7 @@
 
 (in-package #:aoc2024.day2)
 
-(defun parse (filename)
-  (loop :for line :in (uiop:read-file-lines filename)
-        :for diff = nil
-        :for prev = nil
-        :do (cl-ppcre:do-register-groups ((#'parse-integer i)) ("(\\d+)" line)
-              (when prev (push (- i prev) diff))
-              (setf prev i))
-        :collect diff))
-
-(defun safe (diffs &optional (tolerance 0))
+(defun safe-p (diffs &optional (tolerance 0))
   (labels ((unsafe (p d n tol)
              (cond ((null d) nil)
                    ((<= 1 d 3) (unsafe (cons d p) (car n) (cdr n) tol))
@@ -28,7 +19,12 @@
         (not (unsafe nil (- (car diffs)) (mapcar #'- (cdr diffs)) tolerance)))))
 
 (defun solve (path)
-  (loop :for diff :in (parse (truename path))
-        :count (safe diff) :into part1
-        :count (safe diff 1) :into part2
+  (loop :for line :in (uiop:read-file-lines path)
+        :for diff = nil
+        :for prev = nil
+        :do (cl-ppcre:do-register-groups ((#'parse-integer i)) ("(\\d+)" line)
+              (when prev (push (- i prev) diff))
+              (setf prev i))
+        :count (safe-p diff) :into part1
+        :count (safe-p diff 1) :into part2
         :finally (return (values part1 part2))))
