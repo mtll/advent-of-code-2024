151c5252fb378189ec8613a06e43fbc1a3c29150
david feller <dave.feller@gmail.com>
2024-12-05 06:44:41 -0700

    golf

--- Common Lisp/day5.lisp
+++ Common Lisp/day5.lisp
@@ -5,28 +5,21 @@
 
 (in-package #:aoc2024.day5)
 
-(defun parse (path)
-  (let ((table (make-hash-table)))
-    (destructuring-bind (rules lines)
-        (cl-ppcre:split "\\n\\n" (uiop:read-file-string path))
-      (pre:do-register-groups ((#'parse-integer a)
-                               (#'parse-integer b))
-          ("(\\d+)\\|(\\d+)" rules)
-        (push a (gethash b table)))
-      (values (lambda (a b)
-                (not (member b (gethash a table))))
-              (loop :for line :in (pre:split "\\n" lines)
-                    :for result = nil
-                    :do (pre:do-matches-as-strings (a "\\d+" line)
-                          (push (parse-integer a) result))
-                    :collect (nreverse result))))))
-
-(defun solve (path)
-  (multiple-value-bind (ord lines) (parse path)
-    (loop :for line :in lines
-          :for sorted = (sort (copy-seq line) ord)
-          :if (equal line sorted)
-            :sum (nth (floor (length line) 2) line) :into part1
+(defun solve (path &aux (table (make-hash-table)))
+  (destructuring-bind (rules lines)
+      (cl-ppcre:split "\\n\\n" (uiop:read-file-string path))
+    (pre:do-register-groups ((#'parse-integer a) (#'parse-integer b))
+        ("(\\d+)\\|(\\d+)" rules)
+      (push a (gethash b table)))
+    (loop :with ord = (lambda (a b) (member b (gethash a table)))
+          :with sorted = nil
+          :for line :in (pre:split "\\n" lines)
+          :for unsorted = nil
+          :do (pre:do-matches-as-strings (a "\\d+" line)
+                (push (parse-integer a) unsorted))
+              (setf sorted (sort (copy-seq unsorted) ord))
+          :if (equal unsorted sorted)
+            :sum (nth (floor (length unsorted) 2) unsorted) :into part1
           :else
-            :sum (nth (floor (length line) 2) sorted) :into part2
+            :sum (nth (floor (length sorted) 2) sorted) :into part2
           :finally (return (values part1 part2)))))
