519b743c5dc2de11c08d410d77e30f5e7e9013f5
david feller <dave.feller@gmail.com>
2024-12-06 14:58:17 -0700

    day 6

---
+++ Mathematica/day6.m
@@ -0,0 +1,44 @@
+(* ::Package:: *)
+
+SetDirectory[NotebookDirectory[]];
+
+
+(* ::Title:: *)
+(*2024 Day 6*)
+
+
+input=Characters@ReadList["../input/6.txt", "String"];
+obs = Complex@@@Position[input, "#"];
+start=Complex@@FirstPosition[input, "^"] -> -1;
+
+
+floorQ[p_ -> d_] := {1, 1} \[VectorLessEqual] ReIm[p+d] \[VectorLessEqual] Dimensions[input];
+obsQ[p_,d_] := MemberQ[obs, p+d];
+walk[p_->d_] := If[obsQ[p, d] ,p->d*-I, p+d -> d];
+
+
+(* ::Subsection:: *)
+(*Part 1*)
+
+
+path = NestWhileList[walk, start, floorQ];
+Length[<|path|>]
+
+
+(* ::Subsection:: *)
+(*Part 2*)
+
+
+loopQ[ob_]:= Catch@Block[{obs=Append[obs, ob], obsQ},
+	obsQ[p_, d_] := If[MemberQ[obs, p+d],
+		(obsQ[p, d] := Throw[True]);True,
+		obsQ[p, d] = False];
+	NestWhile[walk, start, floorQ];
+	False];
+
+
+(* ::Subsubsection:: *)
+(*It's the CPU's Problem Now*)
+
+
+Parallelize[loopQ /@ DeleteDuplicates[Plus@@#& /@ path]] // Count[True]
