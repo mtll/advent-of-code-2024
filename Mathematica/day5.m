13271f0ff9b65ec4568a46c1a3a7e823b0c80b1f
david feller <dave.feller@gmail.com>
2024-12-05 19:22:06 -0700

    Add more correct solution

--- Mathematica/day5.m
+++ Mathematica/day5.m
@@ -8,6 +8,12 @@ SetDirectory[NotebookDirectory[]];
 
 
 {rs, lines} = SequenceSplit[ToExpression/@Import["../input/5.txt", "Data"],{{}}];
+
+
+(* ::Section:: *)
+(*Less Correct*)
+
+
 ord[a_, b_] := ord[a, b] = Not@MemberQ[rs, {_[b, a]}];
 
 
@@ -16,3 +22,22 @@ ord[a_, b_] := ord[a, b] = Not@MemberQ[rs, {_[b, a]}];
 
 
 Total[If[OrderedQ[#, ord], {#[[\[LeftCeiling]Length@#/2\[RightCeiling]]], 0}, {0, Sort[#, ord][[\[LeftCeiling]Length@#/2\[RightCeiling]]]}]& /@ lines]
+
+
+(* ::Section:: *)
+(*More Correct*)
+
+
+g = Graph[rs /. {_[a_,b_]} :> a -> b];
+
+
+(* ::Subsection:: *)
+(*Part 1 & 2*)
+
+
+val[l_] := Module[{ord = TopologicalSort[Subgraph[g, l]], s},
+	s = Permute[l, Flatten@MapIndexed[FirstPosition[ord, #1, #2]&, l]];
+	If[s == l, {l[[\[LeftCeiling]Length@l/2\[RightCeiling]]], 0}, {0, s[[\[LeftCeiling]Length@s/2\[RightCeiling]]]}]]
+
+
+Total[val/@lines]
