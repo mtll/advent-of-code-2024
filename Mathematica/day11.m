a72a0a8a852c052bd7d517b376980d0d7eb63fbb
david feller <dave.feller@gmail.com>
2024-12-11 14:57:42 -0700

    cleanup

--- Mathematica/day11.m
+++ Mathematica/day11.m
@@ -7,26 +7,25 @@ SetDirectory[NotebookDirectory[]];
 (*2024 Day 11*)
 
 
-input=ToExpression@StringSplit@ReadString["../input/11.txt"];
+input = ReadList["../input/11.txt", Number];
 
 
-step[n_, 0] = 1;
-step[n_, i_] := step[n, i] = step[n*2024, i-1]
-step[0, i_] := step[0, i] = step[1, i-1];
-step[n_, i_] := 
-	Total[step[#,i-1]&/@QuotientRemainder[n, \!\(\*SuperscriptBox[\(10\), 
-FractionBox[\(\[LeftCeiling]Log[10, \ n + 1]\[RightCeiling]\), \(2\)]]\)]] /; EvenQ[\[LeftCeiling]Log[10, n+1]\[RightCeiling]]
+blk[n_, 0] = 1;
+blk[n_, i_] := blk[n, i] = blk[n*2024, i-1]
+blk[0, i_] := blk[0, i] = blk[1, i-1];
+blk[n_ /; EvenQ[\[LeftCeiling]Log[10, n+1]\[RightCeiling]], i_] :=
+	blk[n, i] = Sum[blk[m, i-1], {m, QuotientRemainder[n, \!\(\*SuperscriptBox[\(10\), FractionBox[\(\[LeftCeiling]Log[10, \ n + 1]\[RightCeiling]\), \(2\)]]\)]}]
 
 
 (* ::Subsection:: *)
 (*Part 1*)
 
 
-Sum[step[n, 25], {n, input}]
+Sum[blk[n, 25], {n, input}]
 
 
 (* ::Subsection:: *)
 (*Part 2*)
 
 
-Sum[step[n, 75], {n, input}]
+Sum[blk[n, 75], {n, input}]
