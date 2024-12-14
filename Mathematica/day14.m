0b293723779a84b68e95abc7833e6a5864901b54
david feller <dave.feller@gmail.com>
2024-12-14 13:14:51 -0700

    use cbt

--- Mathematica/day14.m
+++ Mathematica/day14.m
@@ -18,7 +18,7 @@ step[{x_, y_},{dx_, dy_}] := {Mod[x+dx, 101], Mod[y+dy, 103]}
 (*Part 1*)
 
 
-safety[ps_]:=
+safety[ps_] :=
 	Count[ps, x_ /; x \[VectorLess] {50, 51}, 1] *
 	Count[ps, x_ /; {50, -\[Infinity]} \[VectorLess] x \[VectorLess] {\[Infinity], 51}, 1] *
 	Count[ps, x_ /; {-\[Infinity], 51} \[VectorLess] x \[VectorLess] {50, \[Infinity]}, 1] *
@@ -32,7 +32,7 @@ safety@Nest[Thread[step[#, vs]]&, ps, 100]
 (*Part 2*)
 
 
-{p2} = Ordering[Variance /@ NestList[Thread[step[#, vs]]&, ps, 101*103], 1] - 1
+p2 = ChineseRemainder[First@Ordering[#,1]& /@ Transpose[Variance /@ NestList[Thread[step[#, vs]]&, ps, 103]],{101, 103}] - 1
 
 
-ArrayPlot@SparseArray[#+1 -> 1& /@ Nest[Thread[f[#, vs]]&, ps, p2]]
+ArrayPlot@SparseArray[#+1 -> 1& /@ Nest[Thread[step[#, vs]]&, ps, p2]]
